052c0c529cc842143ec916635715123717f0c4d2
Cassanelli <daniel.bodignon@gmail.com>
2021-09-16 08:49:51 -0300

    Init commit

---
+++ src/test/kotlin/io/lab/sample/springkotlin/SpringkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.lab.sample.springkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
